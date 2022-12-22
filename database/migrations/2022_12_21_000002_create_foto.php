d89fce04f88b1eafe93f813ae09a43235e0ae42d
Eduardito187 <eduardchavez302@gmail.com>
2022-12-22 00:27:20 -0400

    send migrations

--- database/migrations/2022_12_21_000002_create_foto.php
+++ database/migrations/2022_12_21_000002_create_foto.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateFoto extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('foto', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('direccion_imagen');
+            $table->string('url_imagen');
+          
         });
     }
 
+  
     /**
      * Reverse the migrations.
      *
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('foto');
     }
 }
